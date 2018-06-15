6f76f78104b3e9f4b9225e8e7fcf29f2767937f7
9LyCode <40172920+9LyCode@users.noreply.github.com>
2018-06-15 12:00:18 -0400

    Update info.ts

--- src/cmd/utility_cmds/info.ts
+++ src/cmd/utility_cmds/info.ts
@@ -1,4 +1,4 @@
-/*
+
 import { DiscordClientWrapper } from '../../DiscordClientWrapper';
 import * as Discord from 'discord.js';
 import { version } from 'discord.js';
@@ -23,18 +23,17 @@ export class Info extends Command {
         ]
     };
 
-    run(bot: DiscordClientWrapper, msg: Discord.Message, params: string[], perms: number) {
-        msg.channel.send(`= PUBG Bot Information =
-• Owner       :: Thomas Ortiz
-• Github      :: https://github.com/Tdortiz/PUBG-Discord-Bot
-• Mem Usage   :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-• Users       :: ${bot.users.size.toLocaleString()}
-• Servers     :: ${bot.guilds.size.toLocaleString()}
-• Channels    :: ${bot.channels.size.toLocaleString()}
-• Discord.js  :: v${version}
-• Typescript  :: v2.8.3
-• Node        :: ${process.version}`, { code: 'asciidoc' });
-    };
+//    run(bot: DiscordClientWrapper, msg: Discord.Message, params: string[], perms: number) {
+//        msg.channel.send(`= PUBG Bot Information =
+//• Owner       :: Thomas Ortiz
+//• Github      :: https://github.com/Tdortiz/PUBG-Discord-Bot
+//• Mem Usage   :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+//• Users       :: ${bot.users.size.toLocaleString()}
+//• Servers     :: ${bot.guilds.size.toLocaleString()}
+//• Channels    :: ${bot.channels.size.toLocaleString()}
+//• Discord.js  :: v${version}
+//• Typescript  :: v2.8.3
+//• Node        :: ${process.version}`, { code: 'asciidoc' });
+//    };
 
 }
-*/
