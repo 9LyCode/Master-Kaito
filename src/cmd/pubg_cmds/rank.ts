1d128693d13ad4a5e3726ec2694c662e9f93085a
9LyCode <40172920+9LyCode@users.noreply.github.com>
2018-06-15 14:10:51 -0400

    Update rank.ts

--- src/cmd/pubg_cmds/rank.ts
+++ src/cmd/pubg_cmds/rank.ts
@@ -110,15 +110,15 @@ export class Rank extends Command {
     addEmbedFields(embed: Discord.RichEmbed, squadType, playerData): void {
         embed.addBlankField(false)
             //.addField(squadType + ' Rank / Rating / Top % / Grade', playerData.rank + ' / ' + playerData.rating + ' / ' + playerData.topPercent + ' / ' + playerData.grade, false)
-            .addField(squadType + 'Rank: ' + playerData.rank, `**Rating: ** ${playerData.rating}             **KD: ** ${playerData.kd}`, false)
+            .addField(squadType + ' Rank: ' + playerData.rank, '**Rating: ** + playerData.rating', false)
             //.addField('Top %', playerData.topPercent, true)            
             .addField('KD / KDA', playerData.kd + ' / ' + playerData.kda, true)
+            .addField('Average Damage', playerData.average_damage_dealt, true);
             .addField('Win %', playerData.winPercent, true)
             .addField('Top 10%', playerData.topTenPercent, true)
             .addField('Headshot Kill %', playerData.headshot_kills, true)
             .addField('Longest Kill', playerData.longest_kill, true)
-            .addField('Average Damage', playerData.average_damage_dealt, true);
-    }
+   }
 
     async checkParameters(msg: Discord.Message, checkSeason: string, checkRegion: string, checkMode: string): Promise<boolean> {
         let errMessage: string = '';
