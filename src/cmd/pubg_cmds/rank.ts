4ee2472f229950b09413d65f7125d7935b56680a
9LyCode <40172920+9LyCode@users.noreply.github.com>
2018-06-15 12:46:34 -0400

    Update rank.ts

--- src/cmd/pubg_cmds/rank.ts
+++ src/cmd/pubg_cmds/rank.ts
@@ -110,7 +110,7 @@ export class Rank extends Command {
     addEmbedFields(embed: Discord.RichEmbed, squadType, playerData): void {
         embed.addBlankField(false)
             //.addField(squadType + ' Rank / Rating / Top % / Grade', playerData.rank + ' / ' + playerData.rating + ' / ' + playerData.topPercent + ' / ' + playerData.grade, false)
-            .addField(squadType + ' Rank: ' + playerData.rank, '-  ', false)
+            .addField(squadType + ' Rank: ' + playerData.rank, null, false)
             .addField('Rating', playerData.rating, true)
             .addField('Top Ten %', playerData.topPercent, true)            
             .addField('KD / KDA', playerData.kd + ' / ' + playerData.kda, true)
